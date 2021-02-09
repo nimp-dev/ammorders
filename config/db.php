363d43d0dcb116340c81e8d802c084738742f6ea
egor <egorkonopka93@gmail.com>
2021-02-09 16:23:57 +0200

    1

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=dashbord',
+    'username' => 'ekonopka',
+    'password' => '36369367',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
